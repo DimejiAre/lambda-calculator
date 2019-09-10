ffe1e15ae2004132bcb55212b5e147a3ee490924
DimejiAre <olardi2@gmail.com>
2019-09-10 12:55:33 +0100

    add basic functionality for Number component

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  const { digit, pressNumber } = props;
   return (
-    <>
+    <button onClick={event => pressNumber}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {digit}
+    </button>
   );
 };
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 
 //Import your array data to from the provided data file
+import { numbers } from "../../../data";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [number, setNumber] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {number.map(digit => {
+         <NumberButton 
+         key={digit}
+         digit={digit}
+         pressNumber={setNumber}/>
+       })}
     </div>
   );
 };
+
+export default Numbers;
