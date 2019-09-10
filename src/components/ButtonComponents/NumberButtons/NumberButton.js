a5fa411052e9daa4d179c403f1a2edbede7d7ffe
DimejiAre <olardi2@gmail.com>
2019-09-10 13:11:53 +0100

    export NumberButton and import into Numbers

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,9 +3,11 @@ import React from "react";
 const NumberButton = (props) => {
   const { digit, pressNumber } = props;
   return (
-    <button onClick={event => pressNumber}>
+    <button onClick={() => pressNumber}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {digit}
     </button>
   );
 };
+
+export default NumberButton;
