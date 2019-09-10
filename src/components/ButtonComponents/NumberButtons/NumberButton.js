09c5668e5c5eb1cd981bce8f52b0236d4103add9
DimejiAre <olardi2@gmail.com>
2019-09-11 00:07:50 +0100

    add styling for number 0 button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const NumberButton = (props) => {
   const { digit, pressNumber } = props;
   return (
-    <button className={"number"} onClick={() => pressNumber}>
+    <button id ={'div' + digit} className={"number"} onClick={() => pressNumber}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
       {digit}
     </button>
